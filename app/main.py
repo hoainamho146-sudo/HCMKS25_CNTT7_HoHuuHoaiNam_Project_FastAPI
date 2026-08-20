961b25d780bed5758998e56a00da2404fe405d03
Hồ  Hữu Hoài Nam <hoainamho146@gmail.com>
2026-08-20 20:27:46 +0700

    Ngày 1/ Task 6: Tạo bảng/migration ban đầu và kiểm tra DB khởi tạo thành công

--- app/main.py
+++ app/main.py
@@ -1,9 +1,13 @@
 from fastapi import FastAPI
+from app.db.database import Base, engine
+from app.models import user, project, project_members, task
 
 app = FastAPI(
     title="Project Management API",
     version="1.0.0"
 )
+
+Base.metadata.create_all(bind=engine)
 
 @app.get("/")
 def root():
